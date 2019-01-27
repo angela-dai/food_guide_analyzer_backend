f4e639bbbafb4661b63e57876c34befcf1bf8d68
Jasper Chan <jasperchan515@gmail.com>
2019-01-26 16:06:18 -0800

    Add post support

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3001
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/submission', (req, res) => {
+    var img = req.body.img;
+    if(!img) {
+        res.send('Error, no data');
+    } else {
+        res.send(img);
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
